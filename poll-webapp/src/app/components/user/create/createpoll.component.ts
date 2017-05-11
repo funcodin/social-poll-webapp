d0f3f332ec5d34aac6867c518389967df2a17a35
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-05-10 23:28:50 -0700

    Added create poll blank page

---
+++ poll-webapp/src/app/components/user/create/createpoll.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'create-poll',
+  templateUrl: './createpoll.component.html',
+  styleUrls: ['./createpoll.component.scss']
+})
+export class CreatePollComponent implements OnInit{
+
+  ngOnInit(){
+
+  }
+
+}
